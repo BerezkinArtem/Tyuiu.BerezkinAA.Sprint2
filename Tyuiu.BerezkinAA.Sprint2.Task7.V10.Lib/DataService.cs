93d430c98883a2a8c6655416bd1696b43da0fd96
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-10-16 23:38:46 +0500

    Task7

---
+++ Tyuiu.BerezkinAA.Sprint2.Task7.V10.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BerezkinAA.Sprint2.Task7.V10.Lib
+{
+    public class DataService : ISprint2Task7V10
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((y == x) && (y == -x) && (y == x * x - 2))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+            
+        }
+    }
+}
---
+++ Tyuiu.BerezkinAA.Sprint2.Task7.V10/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.BerezkinAA.Sprint2.Task7.V10.Lib;
+
+namespace Tyuiu.BerezkinAA.Sprint2.Task7.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Березкин А. А. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Березкин Артем Андреевич | ИСПб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает данные и вычисляет        *");
+            Console.WriteLine("* находится ли точка с координатами X,Y в заштрихованной области.         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в закрашенной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в закрашенной области");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
+        
